4958c384cf6133b3a8e4115372e3f73e80b8c58f
harshk9155 <harshk9155@gmail.com>
2026-08-11 15:07:52 +0530

    Update MongoDB connection URL format

--- server/Database/db.js
+++ server/Database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 
  const Connection = async (USERNAME,PASSWORD) => {
-    const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@blog-app.v0gtbds.mongodb.net/?appName=blog-app`;
+   const URL = `mongodb://${USERNAME}:${PASSWORD}@ac-vggrpeb-shard-00-00.v0gtbds.mongodb.net:27017,ac-vggrpeb-shard-00-01.v0gtbds.mongodb.net:27017,ac-vggrpeb-shard-00-02.v0gtbds.mongodb.net:27017/?ssl=true&replicaSet=atlas-1gf77g-shard-0&authSource=admin&appName=blog-app`;
     try {
            await mongoose.connect(URL);
  
